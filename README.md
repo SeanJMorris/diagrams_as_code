2ed5006ff2111203a55b49e354428607b2301332
Sean Morris <seanj.morris@gmail.com>
2025-12-06 23:42:02 -0500

    Make first diagram.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 1. Python's [diagram library](https://diagrams.mingrammer.com/docs/getting-started/installation) requires that [Graphviz](https://www.graphviz.org/download/) is installed in your system. Ensure graphviz is installed with `sudo apt install graphviz`.
 2. Install with uv: `uv tool install diagrams`
+3. Run with `uv run main.py`
--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
-
 def main():
-    print("Hello from diagrams-as-code!")
 
-    with Diagram("Web Service", show=False):
+    file_label = "Sean's First Diagram"
+    # File names automatically get renamed to lowercase with underscores instead of spaces as a png file.
+    file_name = file_label.lower().replace(" ", "_") + ".png"
+
+    print(f"The {file_name} diagram has been generated.")
+
+    with Diagram("Sean's First Diagram", show=False):
         ELB("lb") >> EC2("web") >> RDS("userdb")
 
 if __name__ == "__main__":
